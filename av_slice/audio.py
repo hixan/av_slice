75b05486f1e3c77aa2e0876242bd6a43264148dc
Hixan <alex.88.elias@gmail.com>
2019-06-18 20:40:52 +1000

    changed quiet_sections argument 'chunk_width' to 'chunk_duration' for simplicity

--- av_slice/audio.py
+++ av_slice/audio.py
@@ -1,21 +1,23 @@
 import numpy as np
 
-def quiet_sections(audio_clip, chunk_width, threshold=.01):  # {{{
+def quiet_sections(audio_clip, chunk_duration, threshold=.01):  # {{{
 
-    assert type(chunk_width) is int
     # store sectional data
     silent_sections = []
     current_loud = False
-    for i, chunk in enumerate(audio_clip.iter_chunks(chunksize=chunk_width)):
+    for i, chunk in enumerate(audio_clip.iter_chunks(
+        chunk_duration=chunk_duration,
+        progress_bar=True
+    )):
         a = np.max(chunk)  # use numpy as chunk is a 2d array
         if not current_loud:
             if a >= threshold:
-                start_loud = i * chunk_width / audio_clip.fps
+                start_loud = i * chunk_duration
                 current_loud = True
         else:
             if a < threshold:
                 silent_sections.append(
-                    (start_loud, i * chunk_width / audio_clip.fps)
+                    (start_loud, i * chunk_duration)
                 )
                 current_loud = False
     return silent_sections  # }}}
