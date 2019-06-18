d729afd040faf7c07a214a09ca9be8da2b7b2e0d
Hixan <alex.88.elias@gmail.com>
2019-06-18 20:58:04 +1000

    removed unneccesary return statement

--- av_slice/video.py
+++ av_slice/video.py
@@ -7,8 +7,4 @@ def remove_sections(video_clip, sections):  # {{{
     return editor.concatenate_videoclips(
         [video_clip.subclip(start, end) for start, end in sections]
     )
-    clips = []
-    for start, end in sections:
-        clips.append(video_clip.subclip(start, end))
-    return editor.concatenate_videoclips(clips)
 # }}}
